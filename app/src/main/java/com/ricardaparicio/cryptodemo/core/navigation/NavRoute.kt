3103406292096d085e60621ef99dcaa7e49ecc8b
Ricard Aparicio <ricard.apafal@gmail.com>
2022-02-05 20:39:02 +0100

    navigation route class

---
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/navigation/NavRoute.kt
@@ -0,0 +1,31 @@
+package com.ricardaparicio.cryptodemo.core.navigation
+
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+sealed class NavRoute(
+    private val route: String,
+    private val navArgs: List<NavArg> = emptyList(),
+) {
+    
+    object CoinDetail : NavRoute("coinDetail", listOf(NavArg.CoinId))
+
+    val args: List<NamedNavArgument> = navArgs.map { navArg ->
+        navArgument(navArg.key) {
+            type = navArg.type
+        }
+    }
+
+    val destination: String = run {
+        val argKeys = navArgs.map { navArg -> "{${navArg.key}}" }
+        buildList {
+            add(route)
+            addAll(argKeys)
+        }.joinToString("/")
+    }
+}
+
+enum class NavArg(val key: String, val type: NavType<*>) {
+    CoinId("coinId", NavType.StringType),
+}
