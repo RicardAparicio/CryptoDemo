be0f053b032ce627b062b0cda8fc9c97ffd0e6b0
Ricard Aparicio <ricard.apafal@gmail.com>
2022-02-15 16:49:40 +0100

    UI and proper number formats

--- app/src/main/java/com/ricardaparicio/cryptodemo/core/util/NumberFormatExt.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/core/util/NumberFormatExt.kt
@@ -1,18 +1,24 @@
 package com.ricardaparicio.cryptodemo.core.util
 
 import com.ricardaparicio.cryptodemo.features.common.domain.model.FiatCurrency
+import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
 
-fun Float.formatPrice(fiatCurrency: FiatCurrency): String {
-    val format: NumberFormat = NumberFormat.getCurrencyInstance()
-    format.currency = Currency.getInstance(
-        when (fiatCurrency) {
-            FiatCurrency.Eur -> "EUR"
-            FiatCurrency.Usd -> "USD"
-        }
-    )
-    return format.format(this)
-}
+fun Float.formatPrice(fiatCurrency: FiatCurrency): String =
+    NumberFormat.getCurrencyInstance().run {
+        currency = Currency.getInstance(
+            when (fiatCurrency) {
+                FiatCurrency.Eur -> "EUR"
+                FiatCurrency.Usd -> "USD"
+            }
+        )
+        format(this@formatPrice)
+    }
 
-fun Float.formatPercentage(): String = NumberFormat.getPercentInstance().format(this)
+fun Float.formatPercentage(): String =
+    NumberFormat.getPercentInstance().run {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+        format(this@formatPercentage)
+    }
--- app/src/main/java/com/ricardaparicio/cryptodemo/features/common/ui/viewmodel/CommonViewModelModule.kt
+++ app/src/main/java/com/ricardaparicio/cryptodemo/features/common/ui/viewmodel/CommonViewModelModule.kt
@@ -4,9 +4,6 @@ import com.ricardaparicio.cryptodemo.core.Reducer
 import com.ricardaparicio.cryptodemo.features.common.ui.ContentLoadingUiState
 import com.ricardaparicio.cryptodemo.features.common.ui.reducer.ContentLoadingReducer
 import com.ricardaparicio.cryptodemo.features.common.ui.reducer.ContentLoadingUiAction
-import com.ricardaparicio.cryptodemo.features.detail.ui.CoinDetailUiState
-import com.ricardaparicio.cryptodemo.features.detail.ui.reducer.CoinDetailReducer
-import com.ricardaparicio.cryptodemo.features.detail.ui.reducer.CoinDetailUiAction
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
